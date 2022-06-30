0411ca1c16a7d9241a276bcf9e67b99f4240bd03
Timothee Odushina <hsp20tim@gmail.com>
2022-06-30 10:44:38 -0600

    added index

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+
+let userBooks = prompt("How many books have you read this month?");
+
+if (userBooks >=1) {
+    document.write("Gerat Job! Keep it up!");
+} else {
+    document.write("Check some of my favorite books");
+}
